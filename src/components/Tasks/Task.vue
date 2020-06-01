a1f25a0f9a040d870f1707aaa650c3f3879955b4
SeminVsevolod <semin.vsevolod@gmail.com>
2020-06-01 19:36:39 +0300

    refactor task to separate component

---
+++ src/components/Tasks/Task.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-item
+    v-ripple
+    clickable
+    :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
+    @click="task.completed = !task.completed"
+  >
+    <q-item-section
+      side
+      top
+    >
+      <q-checkbox v-model="task.completed" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label :class="{'task-strikethrough': task.completed}">
+        {{ task.name }}
+      </q-item-label>
+    </q-item-section>
+
+    <q-item-section
+      side
+    >
+      <div class="row">
+        <div class="column justify-center">
+          <q-icon
+            class="q-mr-xs"
+            name="event"
+            size="18px"
+          />
+        </div>
+        <div class="column items-end">
+          <q-item-label caption>
+            {{ task.dueDate }}
+          </q-item-label>
+          <q-item-label caption>
+            <small>{{ task.dueTime }}</small>
+          </q-item-label>
+        </div>
+      </div>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+    id: {
+      type: [Number, String],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -4,49 +4,12 @@
       separator
       bordered
     >
-      <q-item
+      <task
         v-for="(task, taskKey) in tasks"
+        :id="taskKey"
         :key="taskKey"
-        v-ripple
-        clickable
-        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-        @click="task.completed = !task.completed"
-      >
-        <q-item-section
-          side
-          top
-        >
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label :class="{'task-strikethrough': task.completed}">
-            {{ task.name }}
-          </q-item-label>
-        </q-item-section>
-
-        <q-item-section
-          side
-        >
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon
-                class="q-mr-xs"
-                name="event"
-                size="18px"
-              />
-            </div>
-            <div class="column items-end">
-              <q-item-label caption>
-                {{ task.dueDate }}
-              </q-item-label>
-              <q-item-label caption>
-                <small>{{ task.dueTime }}</small>
-              </q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+        :task="task"
+      />
     </q-list>
   </q-page>
 </template>
@@ -56,6 +19,9 @@ import { mapGetters } from 'vuex';
 
 export default {
   name: 'PageTodo',
+  components: {
+    Task: () => import('../components/Tasks/Task.vue'),
+  },
   computed: {
     ...mapGetters('tasks', ['tasks']),
   },
