18e7d172c866b9420ae6f2446e1e96bd6948e1f1
SeminVsevolod <semin.vsevolod@gmail.com>
2020-06-06 00:11:35 +0300

    setup firebase connection via .env

---
+++ src/boot/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAuth = firebaseApp.auth();
+export { firebaseAuth };
